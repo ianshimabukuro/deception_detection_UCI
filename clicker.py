3c66414d9905285cfe6cb2496b2a8610150b5ec4
Ian Shimabukuro <shimakun000@gmail.com>
2025-06-02 21:53:46 -0700

    Changed left and right

--- clicker.py
+++ clicker.py
@@ -1,5 +1,6 @@
 import time
 import keyboard
+import json
 
 FPS = 24
 isRunning = False
@@ -7,10 +8,10 @@ clicker_list = []
 print("Press space bar to start ")
 keyboard.wait('space')
 while True:
-    if keyboard.is_pressed('left'):
+    if keyboard.is_pressed('right'):
         clicker_list.append("True")
         print("True")
-    elif keyboard.is_pressed('right'):
+    elif keyboard.is_pressed('left'):
         clicker_list.append("False")
         print("False")
     else:
@@ -19,6 +20,12 @@ while True:
     time.sleep(1/FPS)
     if keyboard.is_pressed('esc'):
         break 
+
+
+with open("clicker.json","w") as f:
+    json.dump(clicker_list,f,indent = 2)
+
+
 
 print(len(clicker_list))
     
